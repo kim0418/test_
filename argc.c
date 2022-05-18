bdeb326e56ff06f1fd3a9e17579ae593cc5abb84
kim0418 <ing1821@daum.net>
2022-05-17 23:07:26 -0700

    argc15

--- argc.c
+++ argc.c
@@ -18,7 +18,6 @@ void file_list(char *argv){
 	char str[100];
 	while(fgets(curr,100,fp1)){
 		fputs(curr,fp2);
-		
 	}
 	fclose(fp1);
 	fclose(fp2);
@@ -45,13 +44,50 @@ int line_count(char *argv){
 void file_replace(char* argv){
     FILE *fp1=fopen(argv,"r");
     FILE *fp2=fopen("copy2.txt","a");
-    int count;
-    int find;
+    int count=0;
     char str[100];
-    char*p;
-    
-    
+    char cur[100];
+    while(!feof(fp1)){
+    	fgets(str,100,fp1);
+    	char*p=strstr(str,"int ");
+    	if(p){
+    		strncpy(p,"double ",7);
+    		count++;
+    	}
+    	fputs(str,fp2);
+    }
+    fclose(fp1);
+    fclose(fp2);
+
+    FILE*fp3=fopen("copy2.txt","r");
+    while((fgets(cur,100,fp3)!=NULL)){
+	printf("%s\n",cur);
+    }
+    fclose(fp3);
     printf("총 %d개의 int가 double로 변경되었습니다.\n",count);
+}
+
+void analysis(char* argv){
+	int count1=0;
+	int count2=0;
+	char str[100];
+	FILE*fp=fopen(argv,"r");
+	while(!feof(fp)){
+		fgets(str,100,fp);
+		if(strstr(str,"int ")!=NULL){
+			//printf("%s\n",str);
+			count1++;
+		}
+		if(strstr(str,"double ")!=NULL){
+			//printf("%s\n",str);
+			count1++;
+		}
+		else if(strstr(str,"void")!=NULL){
+			count2++;
+		}
+	}
+	printf("본 코드에 사용된 변수 %d개\n", count1);
+	printf("본 코드에 사용된 함수 %d개\n", count2);
 }
 
 void option_handler(OPTION opt, char* argv){
@@ -61,10 +97,10 @@ void option_handler(OPTION opt, char* argv){
 	    file_list(argv);
 	    break;
 	case HELP:
-	    printf("file.exe -list file-name  ==  \n");
-	    printf("file.exe -listcount file-name  == file linecount \n");
-	    printf("file.exe -replace file-name int double == change int->double \n");
-	    printf("file.exe -analysis file-name  ==  \n");
+	    printf("file.exe -list file-name  == 파일 내용 확인하기\n");
+	    printf("file.exe -listcount file-name  == 파일 총 라인갯수 확인하기 \n");
+	    printf("file.exe -replace file-name int double == 파일내 int를 double로 변환 \n");
+	    printf("file.exe -analysis file-name  ==  사용된 변수와 함수의 개수 출력하기\n");
 	    break;
 	case LISTCOUNT:
 	    printf("code line is = %d\n",line_count(argv));
@@ -73,6 +109,7 @@ void option_handler(OPTION opt, char* argv){
 	    file_replace(argv);
 	    break;
 	case ANALYSIS:
+	    analysis(argv);
 	    break;
     }
 }
@@ -86,7 +123,7 @@ int main(int argc,char * argv[]){
 	printf("ARG %d = %s\n",i,argv[i]);
     }
 	printf("\n");
-    if(argc==1){
+    if(argv[1]==NULL){
     	printf("insert option\n");
     }
     if(strcmp(argv[1],"-list") == 0){
